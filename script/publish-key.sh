17744a88ed8591c4d6abf74afc6c3ada48b73cdb
Eric Lin <linyang0625@users.noreply.github.com>
2018-05-14 00:32:21 +0800

    Update publish-key.sh

--- script/publish-key.sh
+++ script/publish-key.sh
@@ -1,36 +1,13 @@
 #!/bin/bash
-yum -y install expect
+yum install -y sshpass
 
 sshkey_home="/root/jenkins_sshkey"
 rm -rf $sshkey_home
 mkdir $sshkey_home
 ssh-keygen -t rsa -f "$sshkey_home/id_rsa" -N ""
 
-#at $sshkey_home/id_rsa.pub >> /root/.ssh/authorized_keys
-#ssh -o StrictHostKeyChecking=no -i $sshkey_home/id_rsa root@127.0.0.1
-#echo "Login self success!"
-
-while read ip pwd; do 
-#expect scp_pub.exp $ip $pwd "$sshkey_home/id_rsa.pub"
-expect << EOF
-spawn scp -o StrictHostKeyChecking=no $sshkey_home/id_rsa.pub root@$ip:/root/my_id_rsa.pub
-expect "password:"
-send "$pwd\r"
-expect eof
-EOF
-
-expect << EOF
-    spawn ssh root@$ip
-    expect "password:"
-    send "$pwd\r"
-    expect "*#*" {send "cat /root/my_id_rsa.pub >> /root/.ssh/authorized_keys && rm -f /root/my_id_rsa.pub\r"}
-    expect "*#*"
-    send "exit\r"
-    expect eof
-    spawn ssh -i "$sshkey_home/id_rsa" root@$ip
-    expect "]#" {send "echo 'Login $ip using ssh key success!'\r"}
-    expect "]#" {send "exit\r"}
-    expect eof
-EOF
-done < servers.txt
-
+while read ip pwd
+do 
+#echo "$ip || $pwd"
+sshpass -p $pwd ssh-copy-id -o StrictHostKeyChecking=no -i $sshkey_home/id_rsa.pub root@$ip
+done < server.txt 
